82f73bf8ec82178f75a89e43ab46b2b52c4f2378
Jaewon Yun <woin2ee@gmail.com>
2024-07-18 21:08:26 +0900

    Implement first

--- Sources/FIFOQueue/FIFOQueue.swift
+++ Sources/FIFOQueue/FIFOQueue.swift
@@ -1,2 +1,80 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+public struct FIFOQueue<Element> {
+    
+    class Node {
+        let content: Element
+        var next: Node?
+        var previous: Node?
+        
+        init(content: Element) {
+            self.content = content
+            self.next = nil
+            self.previous = nil
+        }
+    }
+    
+    private var first: Node?
+    
+    private var last: Node?
+    
+    public let maxCapacity: Int
+    
+    public var count: Int
+    
+    private let lock: NSRecursiveLock = NSRecursiveLock()
+    
+    public init(maxCapacity: Int) {
+        self.maxCapacity = maxCapacity
+        self.count = 0
+    }
+    
+    /// Enqueues the element.
+    ///
+    /// If the `enqueue` causes it to exceed the maximum capacity, it's returned the element that enqueued very first.
+    @discardableResult
+    public mutating func enqueue(_ element: Element) -> Element? {
+        lock.lock(); defer { lock.unlock() }
+        
+        let node = Node(content: element)
+        first?.previous = node
+        node.next = first
+        first = node
+        
+        if last == nil {
+            last = node
+        }
+        
+        count += 1
+        
+        if count > maxCapacity {
+            return dequeue()
+        }
+        
+        return nil
+    }
+    
+    /// Returns very first enqueuing element.
+    @discardableResult
+    public mutating func dequeue() -> Element? {
+        lock.lock(); defer { lock.unlock() }
+        
+        guard let last else {
+            return nil
+        }
+        
+        count -= 1
+        
+        let content = last.content
+        
+        if let previous = last.previous {
+            previous.next = nil
+            self.last = previous
+        } else {
+            self.first = nil
+            self.last = nil
+        }
+        
+        return content
+    }
+}
--- Tests/FIFOQueueTests/FIFOQueueTests.swift
+++ Tests/FIFOQueueTests/FIFOQueueTests.swift
@@ -1,12 +1,133 @@
-import XCTest
 @testable import FIFOQueue
+import XCTest
 
 final class FIFOQueueTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func test_enqueue_dequeue() {
+        var queue = FIFOQueue<Int>(maxCapacity: 10)
+        queue.enqueue(1)
+        XCTAssertEqual(queue.dequeue(), 1)
+    }
+    
+    func test_enqueue_whenOverCapacity() {
+        var queue = FIFOQueue<Int>(maxCapacity: 3)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        let first = queue.enqueue(4)
+        XCTAssertEqual(first, 1)
+    }
+    
+    func test_count() {
+        // Given
+        var queue = FIFOQueue<Int>(maxCapacity: .max)
+        
+        // When
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        
+        // Then
+        XCTAssertEqual(queue.count, 3)
+    }
+    
+    func test_count_whenComplexEnAndDequeue() {
+        // Given
+        var queue = FIFOQueue<Int>(maxCapacity: .max)
+        
+        // When
+        queue.dequeue()
+        queue.enqueue(1)
+        queue.dequeue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.dequeue()
+        queue.dequeue()
+        queue.dequeue()
+        queue.enqueue(1)
+        
+        // Then
+        XCTAssertEqual(queue.count, 1)
+    }
+    
+    func test_count_whenOverMaxCapacity() {
+        // Given
+        var queue = FIFOQueue<Int>(maxCapacity: 3)
+        
+        // When
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        queue.enqueue(4)
+        
+        // Then
+        XCTAssertEqual(queue.count, 3)
+    }
+    
+    func test_lock_withMultiThreading() {
+        // Given
+        var queue = FIFOQueue<Int>(maxCapacity: .max)
+        
+        let expectation = expectation(description: "Enqueue done.")
+        expectation.expectedFulfillmentCount = 20_000
+        
+        let testQueue1 = DispatchQueue(label: "queue1", attributes: .concurrent)
+        let testQueue2 = DispatchQueue(label: "queue2", attributes: .concurrent)
+        
+        // When
+        for _ in (1...10_000) {
+            testQueue1.async {
+                queue.enqueue(1)
+                expectation.fulfill()
+            }
+            testQueue2.async {
+                queue.enqueue(2)
+                expectation.fulfill()
+            }
+        }
+        
+        // Then
+        wait(for: [expectation])
+        XCTAssertEqual(queue.count, 20_000)
+    }
+    
+    func test_recursiveLock_whenOverCapacity_withMultiThreading() {
+        // Given
+        var queue = FIFOQueue<Int>(maxCapacity: 100)
+        var overElements: [Int] = []
+        let lock = NSLock()
+        
+        let expectation = expectation(description: "Enqueue done.")
+        expectation.expectedFulfillmentCount = 20_000
+        
+        let testQueue1 = DispatchQueue(label: "queue1", attributes: .concurrent)
+        let testQueue2 = DispatchQueue(label: "queue2", attributes: .concurrent)
+        
+        // When
+        for _ in (1...10_000) {
+            testQueue1.async {
+                let overElement = queue.enqueue(1)
+                if let overElement {
+                    lock.lock()
+                    overElements.append(overElement)
+                    lock.unlock()
+                }
+                expectation.fulfill()
+            }
+            testQueue2.async {
+                let overElement = queue.enqueue(2)
+                if let overElement {
+                    lock.lock()
+                    overElements.append(overElement)
+                    lock.unlock()
+                }
+                expectation.fulfill()
+            }
+        }
+        
+        // Then
+        wait(for: [expectation], timeout: 5)
+        XCTAssertEqual(queue.count, 100)
+        XCTAssertEqual(overElements.count, 19_900)
     }
 }
